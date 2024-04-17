03db367780e735807ef6ac5925807b4329c563a3
kevinz420 <kzhang0305@gmail.com>
2024-04-16 22:17:29 -0500

    instructions for queries to db

---
+++ frontend/src/services/filterOnAirport.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// write GCP request that returns a list of information regarding flights from `string departureAirport` to `string arrivalAirport` with whatever data fields you deem good to display in the UI
+// ex. return [{data_field1: 'bar', data_field2: 'foo'}, {data_field1: 'bar', data_field2: 'foo'}, {data_field1: 'bar', data_field2: 'foo'}]
+const filterOnAirports = async (departureAirport, arrivalAirport) => {
+
+}
+
+export default filterOnAirports;
---
+++ frontend/src/services/getAirports.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// write GCP request that returns a list of airport names (ex. return ["ORD", "SFO", "MIA"])
+const getAirports = async () => {
+
+}
+
+export default getAirports;
