b781cee4b686857ce7f227c5bbd8d9ebb411876e
Tyler Yu <tyleryu2@illinois.edu>
2024-04-25 15:46:29 -0500

    Clearing repo for large push

--- frontend/src/services/getAirports.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// write GCP request that returns a list of airport names (ex. return ["ORD", "SFO", "MIA"])
-const getAirports = async () => {
-
-}
-
-export default getAirports;
